6b0e05121eaedcd6306eeaf58d3e17075cb89b16
ErenxBKRT <rakotoarinelinalaza@gmail.com>
2026-07-09 19:57:43 +0300

    tsy isy io

--- src/views/Envoyer.tsx
+++ src/views/Envoyer.tsx
@@ -1,8 +1,9 @@
-import Popup from '../shared/Popup'
+//import Popup from '../shared/Popup'
 import '../style/Envoyer.css'
 
 function Envoyer() {
-	return <div style={{width: 'calc(100% - 340px)'}}>
+	return <>
+	<div style={{width: 'calc(100% - 340px)'}}>
 		{/*<Popup content="Supprimer l'element"/>
 		<div className="envoyer-header">
 			<h2>Envoyer</h2>
@@ -10,5 +11,7 @@ function Envoyer() {
 		</div>
 	</div>
 }*/}
+</div>
+</>}
 
 export default Envoyer;
