31601971ed79096882b9d736976151bbba91b879
AntsaIsMe <antsajony@gmail.com>
2026-07-09 15:31:50 +0300

    popup fic

--- src/views/Envoyer.tsx
+++ src/views/Envoyer.tsx
@@ -3,7 +3,6 @@ import '../style/Envoyer.css'
 
 function Envoyer() {
 	return <div style={{width: 'calc(100% - 340px)'}}>
-		<Popup content="Supprimer l'element"/>
 		<div className="envoyer-header">
 			<h2>Envoyer</h2>
 			
