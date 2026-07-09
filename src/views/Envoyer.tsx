fe0e42fafa1fbd9ac44dc4611165dab92bce3fa4
AntsaIsMe <antsajony@gmail.com>
2026-07-09 15:28:47 +0300

    popup done

--- src/views/Envoyer.tsx
+++ src/views/Envoyer.tsx
@@ -1,7 +1,9 @@
+import Popup from '../shared/Popup'
 import '../style/Envoyer.css'
 
 function Envoyer() {
 	return <div style={{width: 'calc(100% - 340px)'}}>
+		<Popup content="Supprimer l'element"/>
 		<div className="envoyer-header">
 			<h2>Envoyer</h2>
 			
